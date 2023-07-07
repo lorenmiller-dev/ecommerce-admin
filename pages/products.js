710deab336eee2bbd26d13a80349038f44dd176d
PreciseJR <jrmiller525@yahoo.com>
2023-07-06 22:22:39 -0700

    fix leak

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,10 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Products() {
     return (
         <Layout>
-            Products page here
+            <Link className="bg-gray-300 text-black rounded-md py-1 px-2" href={'/products/new'}>Add new product</Link>
         </Layout>
     );
 }
