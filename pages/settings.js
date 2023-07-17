c31e64af3271bba9f064a130de9c7ef112ea045a
PreciseJR <jrmiller525@yahoo.com>
2023-07-17 15:55:20 -0700

    Implemented a logout button

--- pages/settings.js
+++ pages/settings.js
@@ -1,5 +1,19 @@
 import Layout from "@/components/Layout";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Settings() {
-  return <Layout>Settings page here</Layout>;
+  return (
+    <Layout>
+      <div className="bg-gray-900 h-screen w-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signOut("google")}
+            className="bg-white p-4 rounded-lg"
+          >
+            logout
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 }
